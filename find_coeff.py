d6201726b0be837cf460f7e886a481fcd6358639
AlexeyST17 <77644863+AlexeyST17@users.noreply.github.com>
2022-04-19 17:03:47 +0300

    Add files via upload

---
+++ find_coeff.py
@@ -0,0 +1,97 @@
+import numpy as np
+import cv2 as cv
+import glob
+
+chessboardSize = (9, 6)     # размер шахматной доски
+frameSize = (780, 517)     # разрешение камеры
+
+# termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# prepare object points, like (0,0,0), (1,0,0)...(6,5,0)
+objp = np.zeros((chessboardSize[0] * chessboardSize[1], 3), np.float32)
+objp[:, :2] = np.mgrid[0:chessboardSize[0], 0:chessboardSize[1]].T.reshape(-1, 2)
+
+# arrays to store object points and image points from all the images
+objpoints = []  # 3d points in real world space
+imgpoints = []  # 2d points in image plane
+
+images = glob.glob('*.jpeg')
+
+for image in images:
+
+    img = cv.imread(image)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    cv.imshow('1', img)
+    #cv.waitKey(1)
+
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, chessboardSize, None)
+
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+
+        objpoints.append(objp)
+        corners2 = cv.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
+        imgpoints.append(corners)
+
+        # Draw and display the corners
+        cv.drawChessboardCorners(img, chessboardSize, corners2, ret)
+        cv.imshow('img', img)
+       # cv.waitKey(1)
+
+
+cv.destroyAllWindows()
+
+
+
+
+############## CALIBRATION #######################################################
+
+ret, cameraMatrix, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, frameSize, None, None)
+
+
+############## UNDISTORTION #####################################################
+
+img = cv.imread('1.jpeg')   # any sample 
+h,  w = img.shape[:2]
+newCameraMatrix, roi = cv.getOptimalNewCameraMatrix(cameraMatrix, dist, (w,h), 1, (w,h))
+
+
+
+# Undistort
+dst = cv.undistort(img, cameraMatrix, dist, None, newCameraMatrix)
+
+# crop the image
+x, y, w, h = roi
+dst = dst[y:y+h, x:x+w]
+cv.imwrite('caliResult1.png', dst)
+
+
+
+# Undistort with Remapping
+mapx, mapy = cv.initUndistortRectifyMap(cameraMatrix, dist, None, newCameraMatrix, (w,h), 5)
+dst = cv.remap(img, mapx, mapy, cv.INTER_LINEAR)
+
+# crop the image
+x, y, w, h = roi
+dst = dst[y:y+h, x:x+w]
+#cv.imwrite('caliResult2.png', dst)
+
+
+
+
+# Reprojection Error
+mean_error = 0
+
+for i in range(len(objpoints)):
+    imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], cameraMatrix, dist)
+    error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
+    mean_error += error
+
+print("\nRMS:", ret)
+print("camera matrix:\n", cameraMatrix)
+print("distortion coefficients: ", dist.ravel())
+print("new camera matrix:\n", newCameraMatrix)
+print("roi:\n", roi)
+print( "total error: {}".format(mean_error/len(objpoints)) )
